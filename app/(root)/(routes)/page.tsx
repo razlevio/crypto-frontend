84a1616eec79bf76bae36916322394b678e1311b
razlevio <razlevio.55@gmail.com>
2024-02-04 13:33:09 +0200

    feat(wrong-dep): push the correct deployment

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
-import { columns, Crypto } from "@/components/columns";
+import { columns } from "@/components/columns";
 import { DataTable } from "@/components/date-table";
 
 export default async function RootPage() {
   const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
   const API_ENDPOINT = `${APP_URL}/api/last`;
+
+  let data = null;
 
   try {
     // Make a fetch request to your endpoint
@@ -18,28 +19,29 @@ export default async function RootPage() {
     }
 
     // Parse the JSON response
-    const data = await res.json();
-    return (
-      <main className="mx-auto max-w-7xl p-6 sm:px-4">
-        <div className="flex items-center justify-center">
-          <Image
-            src="./logo.svg"
-            alt="logo"
-            width={250}
-            height={250}
-            className="size-20"
-          />
-        </div>
-        <div className="container mx-auto py-10">
-          <h1 className="mb-10 text-center text-3xl font-bold">
-            Last Crypto Rates
-          </h1>
-          <DataTable columns={columns} data={data} />
-        </div>
-      </main>
-    );
+    data = await res.json();
   } catch (error) {
     console.error("Failed to fetch data from /api/last", error);
-    return null;
+    return <h1>Problem Fetching Data</h1>
   }
+
+  return (
+    <main className="mx-auto max-w-7xl p-6 sm:px-4">
+      <div className="flex items-center justify-center">
+        <Image
+          src="./logo.svg"
+          alt="logo"
+          width={250}
+          height={250}
+          className="size-20"
+        />
+      </div>
+      <div className="container mx-auto py-10">
+        <h1 className="mb-10 text-center text-3xl font-bold">
+          Last Crypto Rates
+        </h1>
+        <DataTable columns={columns} data={data.rates} />
+      </div>
+    </main>
+  );
 }
